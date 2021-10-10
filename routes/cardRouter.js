175e557c4d10022903ffcf3c9cc1d2b236758acb
Zumry <amzumry@gmail.com>
2021-10-10 22:39:22 +0530

    Card route completed

---
+++ routes/cardRouter.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const Card = require("../models/Card");
+
+const cardRouter = express.Router();
+
+cardRouter.use(bodyParser.json());
+
+cardRouter
+    .route("/")
+    .post(async (req, res, next) => {
+        let card = req.body;
+        await Card.create(card)
+            .then(
+                (category) => {
+                    res.statusCode = 200;
+                    res.setHeader("Content-Type", "application/json");
+                    res.json(category);
+                },
+                (err) => {
+                    next(err);
+                }
+            )
+            .catch((err) => {
+                next(err);
+            });
+    });
+
+cardRouter
+    .route("/")
+    .get(async (req, res, next) => {
+        await Card.find({})
+            .then((card) => {
+                    res.statusCode = 200;
+                    res.setHeader("Content-Type", "application/json")
+                    res.json(card);
+                },
+                (err) => {
+                    next(err);
+                }
+            )
+            .catch((err) => {
+                next(err);
+            });
+
+    });
+
+cardRouter
+    .route("/:id")
+    .get(async (req, res, next) => {
+        await Card.find(req.params.id)
+            .then(
+                (category) => {
+                    res.statusCode = 200;
+                    res.setHeader("Content-Type", "application/json");
+                    res.json(category);
+                },
+                (err) => {
+                    next(err);
+                }
+            )
+            .catch((err) => {
+                next(err);
+            });
+    });
+
+cardRouter
+    .route("/:id")
+    .delete(async (req, res, next) => {
+        await Card.findByIdAndRemove(req.params.id)
+            .then((card) => {
+                res.statusCode = 200;
+                res.setHeader("Content-Type", "application/json");
+                res.json(card);
+            },(err) => {
+                next(err);
+            })
+            .catch((err) => {
+                next(err);
+            })
+    });
+
+module.exports = cardRouter;
