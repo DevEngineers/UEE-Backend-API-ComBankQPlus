b696c0e9c67712bd4ed002adcfbf855909b695c4
Mohamed Akeel <akeel1447@gmail.com>
2021-10-10 21:29:05 +0530

    initial commit

---
+++ routes/billPaymentRouter.js
@@ -0,0 +1,86 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const billPaymentRouter = express.Router();
+
+billPaymentRouter.use(bodyParser.json());
+
+billPaymentRouter
+    .route("/")
+    .get(async (req, res, next) => {
+        /*await ClassTimetable.find({})
+            .populate("class")
+            .populate("classType")
+            .then(
+                (classTimetables) => {
+                    res.statusCode = 200;
+                    res.setHeader("Content-Type", "application/json");
+                    res.json(classTimetables);
+                },
+                (err) => {
+                    next(err);
+                }
+            )
+            .catch((err) => {
+                next(err);
+            });*/
+    })
+    .post(async (req, res, next) => {
+        /*let timetable = req.body;
+        let newArrayObject = createSubjectObject(timetable);
+        let newTimetableOb = changeArrayValues(timetable);
+
+        await ClassTimetable.create(newTimetableOb)
+            .then(
+                (classTimetable) => {
+                    console.log(classTimetable);
+                    ClassTimetable.updateOne(
+                        {_id: classTimetable._id},
+                        {
+                            $set: {
+                                monday: newArrayObject.monday,
+                                tuesday: newArrayObject.tuesday,
+                                wednesday: newArrayObject.wednesday,
+                                thursday: newArrayObject.thursday,
+                                friday: newArrayObject.friday,
+                            },
+                        },
+                        {useFindAndModify: false}
+                    ).then((classTimetable) => {
+                        res.statusCode = 200;
+                        res.setHeader("Content-Type", "application/json");
+                        res.json(classTimetable);
+                    });
+                },
+                (err) => {
+                    next(err);
+                }
+            )
+            .catch((err) => {
+                next(err);
+            });*/
+    });
+
+billPaymentRouter
+    .route("/:id")
+    .get(async (req, res, next) => {
+       /* await ClassTimetable.findById(req.params.id)
+            .populate("class")
+            .populate("classType")
+            .then(
+                (classTimetable) => {
+                    res.statusCode = 200;
+                    res.setHeader("Content-Type", "application/json");
+                    res.json(classTimetable);
+                },
+                (err) => {
+                    next(err);
+                }
+            )
+            .catch((err) => {
+                next(err);
+            });*/
+    });
+
+
+module.exports = billPaymentRouter;
