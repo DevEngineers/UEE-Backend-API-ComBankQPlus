50a5a2ab6cc5408a86b3fbb3212fca3b824ba915
Zumry <amzumry@gmail.com>
2021-10-10 22:56:00 +0530

    Transaction route completed

---
+++ routes/TransactionRouter.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const Transaction = require("../models/Transaction");
+
+const transactionRouter = express.Router();
+
+transactionRouter.use(bodyParser.json());
+
+transactionRouter
+    .route("/")
+    .post(async (req, res, next) => {
+        await Transaction.create(req.body)
+            .then((transaction) => {
+                    res.statusCode = 200;
+                    res.setHeader("Content-Type", "application/json");
+                    res.json(transaction);
+                },
+                (err) => {
+                    next(err);
+                }
+            )
+            .catch((err) => {
+                next(err);
+            });
+    });
+
+transactionRouter
+    .route("/")
+    .get(async (req, res, next) => {
+        await Transaction.find({})
+            .then((transaction) => {
+                    res.statusCode = 200;
+                    res.setHeader("Content-Type", "application/json")
+                    res.json(transaction);
+                },
+                (err) => {
+                    next(err);
+                }
+            )
+            .catch((err) => {
+                next(err);
+            });
+
+    });
+
+transactionRouter
+    .route("/:id")
+    .get(async (req, res, next) => {
+        await Transaction.find(req.params.id)
+            .then(
+                (transaction) => {
+                    res.statusCode = 200;
+                    res.setHeader("Content-Type", "application/json");
+                    res.json(transaction);
+                },
+                (err) => {
+                    next(err);
+                }
+            )
+            .catch((err) => {
+                next(err);
+            });
+    });
+
+
+module.exports = transactionRouter;
